cb6872e36eed8e92253795811a70e0ca32769c49
c-grantham <calgrantham101@gmail.com>
2026-05-04 10:09:05 -0400

    Fix build.sh with tesseract installation

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
+
+# Update and install tesseract
 apt-get update
 apt-get install -y tesseract-ocr tesseract-ocr-eng
+
+# Verify installation
+tesseract --version
+
+# Install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
